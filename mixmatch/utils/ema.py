825403080e345a4283138bd3b71874206a94b8ca
Eve-ning <johnchangqi@hotmail.com>
2023-11-25 19:53:43 +0800

    Format with Black

--- mixmatch/utils/ema.py
+++ mixmatch/utils/ema.py
@@ -15,8 +15,9 @@ class WeightEMA:
         self.ema_model = ema_model
 
     def step(self):
-        for param, ema_param in zip(self.model.parameters(),
-                                    self.ema_model.parameters()):
+        for param, ema_param in zip(
+            self.model.parameters(), self.ema_model.parameters()
+        ):
             if ema_param.dtype == torch.float32:
                 ema_param.mul_(1 - self.ema_lr)
                 ema_param.add_(param * self.ema_lr)
