44ff3b8a7d534991ac63e7d1e2fe0a5db0764af1
Eve-ning <johnchangqi@hotmail.com>
2023-11-26 13:58:15 +0800

    Rename EMA Updater to be clearer on function call

--- mixmatch/utils/ema.py
+++ mixmatch/utils/ema.py
@@ -7,16 +7,20 @@ class WeightEMA:
         self,
         model: nn.Module,
         ema_model: nn.Module,
-        ema_lr: float,
     ):
-        self.ema_lr = ema_lr
         self.model = model
         self.ema_model = ema_model
 
-    def step(self):
+    def update(self, lr: float):
+        """Update the EMA model with the current model's parameters.
+
+        Args:
+            lr: A fraction controlling how much should the EMA learn from the
+                current model.
+        """
         for param, ema_param in zip(
             self.model.parameters(), self.ema_model.parameters()
         ):
             if ema_param.dtype == torch.float32:
-                ema_param.mul_(1 - self.ema_lr)
-                ema_param.add_(param * self.ema_lr)
+                ema_param.mul_(1 - lr)
+                ema_param.add_(param * lr)
